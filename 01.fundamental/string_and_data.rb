32b824f518d8eaa1714930c4201393a4b7b3039a
jmingrove21 <jmingrove21@gmail.com>
2020-04-04 04:37:18 +0900

    string 관련

---
+++ 01.fundamental/string_and_data.rb
@@ -0,0 +1,31 @@
+puts('Hello')
+puts("Hello")
+puts("Hello 'world'")
+puts('Hello "world"')
+
+
+puts('Hello '+'world')
+puts('Hello '*3)
+puts('Hello'[0])
+puts('Hello'[1])
+puts('Hello'[2])
+
+
+
+puts('hello world'.capitalize())
+puts('hello world'.upcase())
+puts('Hello world'.length())
+puts('Hello world'.sub('world', 'programming'))
+
+
+puts("egoing's \"tutorial\"")
+puts("\\")
+puts("Hello\nworld")
+puts("Hello\t\tworld")
+puts("\a")
+puts('Hello\nworld')
+
+
+puts(10+5)
+puts("10"+"5")
+

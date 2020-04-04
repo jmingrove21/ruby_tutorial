7fbc14ce3e3423f6f342a519d93eb0ab2c7c151e
jmingrove21 <jmingrove21@gmail.com>
2020-04-04 16:25:36 +0900

    conditional statement, boolean

---
+++ 02.statement/conditional_statement.rb
@@ -0,0 +1,34 @@
+
+
+if true
+	puts("code1")
+	puts("code2")
+end
+
+puts("code3")
+
+
+input =11
+real =11
+if real == input
+	puts("Hello")
+
+else
+	puts("Who are you?")
+
+end
+
+
+input_t=33
+real_t=11
+real_k="ab"
+
+if real_t==input_t
+	puts("Hello!")
+elsif real_k == real_t
+	puts("Hi!")
+else
+	puts("bye")
+
+end
+
---
+++ 02.statement/readme.md
@@ -0,0 +1,18 @@
+
+<h2> 여러 statement </h2>
+
+<h5> boolean </h5>
+
+* 기존 python과 큰 차이 없음.
+
+* 참, 거짓 표시는 true, false 로 함.
+
+<h5> conditional statement </h5>
+
+* if문 뒤에 소괄호 () 필요 없음.
+
+* 조건문이 끝나면 반드시 end를 써줘야 한다.
+
+* else문은 기존언어들과 동일
+
+* else if는 ** elsif ** 로 표현한다.

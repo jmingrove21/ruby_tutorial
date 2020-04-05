4503b0fbbba517fc9ee61daf961c352c7a669f0f
jmingrove21 <jmingrove21@gmail.com>
2020-04-05 16:16:39 +0900

    let's go function

---
+++ function.rb
@@ -0,0 +1,27 @@
+
+def a3()
+	puts('aaa')
+end
+
+def a4()
+	return 'bbb'
+end
+
+def a5(num)
+	return num+10
+end
+
+def make_string(str, num)
+	return str*num
+end
+
+	
+
+
+a3()
+
+puts(a4())
+
+puts(a5(30))
+
+puts(make_string('HI',3))
